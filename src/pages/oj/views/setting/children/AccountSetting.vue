4bf26f33887c592d5e2bb565e0ddad05c26e1678
FredB-mine <fredbao1126@gmail.com>
2022-11-13 23:01:20 +0800

    Accounts Setting

---
+++ src/pages/oj/views/setting/children/AccountSetting.vue
@@ -0,0 +1,185 @@
+<template>
+  <div class="setting-main">
+    <div class="flex-container">
+      <div class="left">
+        <p class="section-title">{{$t('m.ChangePassword')}}</p>
+        <Form class="setting-content" ref="formPassword" :model="formPassword" :rules="rulePassword">
+          <FormItem :label="$t('m.Old_Password')" prop="old_password">
+            <Input v-model="formPassword.old_password" type="password"/>
+          </FormItem>
+          <FormItem :label="$t('m.New_Password')" prop="new_password">
+            <Input v-model="formPassword.new_password" type="password"/>
+          </FormItem>
+          <FormItem :label="$t('m.Confirm_New_Password')" prop="again_password">
+            <Input v-model="formPassword.again_password" type="password"/>
+          </FormItem>
+          <FormItem v-if="visible.tfaRequired" :label="$t('m.Two_Factor_Auth')" prop="tfa_code">
+            <Input v-model="formPassword.tfa_code"/>
+          </FormItem>
+          <FormItem v-if="visible.passwordAlert">
+            <Alert type="success">{{$t('m.Relogin_In5s')}}</Alert>
+          </FormItem>
+          <Button type="primary" @click="changePassword">{{$t('m.Update_Password')}}</Button>
+        </Form>
+      </div>
+
+      <div class="middle separator"></div>
+
+      <div class="right">
+        <p class="section-title">{{$t('m.ChangeEmail')}}</p>
+        <Form class="setting-content" ref="formEmail" :model="formEmail" :rules="ruleEmail">
+          <FormItem :label="$t('m.Current_Password')" prop="password">
+            <Input v-model="formEmail.password" type="password"/>
+          </FormItem>
+          <FormItem :label="$t('m.Old_Email')">
+            <Input v-model="formEmail.old_email" disabled/>
+          </FormItem>
+          <FormItem :label="$t('m.New_Email')" prop="new_email">
+            <Input v-model="formEmail.new_email"/>
+          </FormItem>
+          <FormItem v-if="visible.tfaRequired" :label="$t('m.Two_Factor_Auth')" prop="tfa_code">
+            <Input v-model="formEmail.tfa_code"/>
+          </FormItem>
+          <Button type="primary" @click="changeEmail">{{$t('m.ChangeEmail')}}</Button>
+        </Form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import api from '@oj/api'
+  import { FormMixin } from '@oj/components/mixins'
+
+  export default {
+    mixins: [FormMixin],
+    data () {
+      const oldPasswordCheck = [{required: true, trigger: 'blur', min: 6, max: 20}]
+      const tfaCheck = [{required: true, trigger: 'change'}]
+      const CheckAgainPassword = (rule, value, callback) => {
+        if (value !== this.formPassword.new_password) {
+          callback(new Error(this.$i18n.t('m.password_does_not_match')))
+        }
+        callback()
+      }
+      const CheckNewPassword = (rule, value, callback) => {
+        if (this.formPassword.old_password !== '') {
+          if (this.formPassword.old_password === this.formPassword.new_password) {
+            callback(new Error(this.$i18n.t('m.Password_No_Change')))
+          } else {
+            // 对第二个密码框再次验证
+            this.$refs.formPassword.validateField('again_password')
+          }
+        }
+        callback()
+      }
+      return {
+        loading: {
+          btnPassword: false,
+          btnEmail: false
+        },
+        visible: {
+          passwordAlert: false,
+          emailAlert: false,
+          tfaRequired: false
+        },
+        formPassword: {
+          tfa_code: '',
+          old_password: '',
+          new_password: '',
+          again_password: ''
+        },
+        formEmail: {
+          tfa_code: '',
+          password: '',
+          old_email: '',
+          new_email: ''
+        },
+        rulePassword: {
+          old_password: oldPasswordCheck,
+          new_password: [
+            {required: true, trigger: 'blur', min: 6, max: 20},
+            {validator: CheckNewPassword, trigger: 'blur'}
+          ],
+          again_password: [
+            {required: true, validator: CheckAgainPassword, trigger: 'change'}
+          ],
+          tfa_code: tfaCheck
+        },
+        ruleEmail: {
+          password: oldPasswordCheck,
+          new_email: [{required: true, type: 'email', trigger: 'change'}],
+          tfa_code: tfaCheck
+        }
+      }
+    },
+    mounted () {
+      this.formEmail.old_email = this.$store.getters.user.email || ''
+    },
+    methods: {
+      changePassword () {
+        this.validateForm('formPassword').then(valid => {
+          this.loading.btnPassword = true
+          let data = Object.assign({}, this.formPassword)
+          delete data.again_password
+          if (!this.visible.tfaRequired) {
+            delete data.tfa_code
+          }
+          api.changePassword(data).then(res => {
+            this.loading.btnPassword = false
+            this.visible.passwordAlert = true
+            this.$success(this.$i18n.t('m.Updated_Successfully'))
+            setTimeout(() => {
+              this.visible.passwordAlert = false
+              this.$router.push({name: 'logout'})
+            }, 5000)
+          }, res => {
+            if (res.data.data === 'tfa_required') {
+              this.visible.tfaRequired = true
+            }
+            this.loading.btnPassword = false
+          })
+        })
+      },
+      changeEmail () {
+        this.validateForm('formEmail').then(valid => {
+          this.loading.btnEmail = true
+          let data = Object.assign({}, this.formEmail)
+          if (!this.visible.tfaRequired) {
+            delete data.tfa_code
+          }
+          api.changeEmail(data).then(res => {
+            this.loading.btnEmail = false
+            this.visible.emailAlert = true
+            this.$success(this.$i18n.t('m.Updated_Successfully'))
+            this.$refs.formEmail.resetFields()
+          }, res => {
+            if (res.data.data === 'tfa_required') {
+              this.visible.tfaRequired = true
+            }
+          })
+        })
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+
+  .flex-container {
+    justify-content: flex-start;
+    .left {
+      flex: 1 0;
+      width: 250px;
+      padding-right: 5%;
+    }
+    > .middle {
+      flex: none;
+    }
+    .right {
+      flex: 1 0;
+      width: 250px;
+    }
+  }
+</style>
+
