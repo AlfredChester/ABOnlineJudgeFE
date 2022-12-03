fb1b771698f971c25af14523ec505b906c57da22
FredB-mine <fredbao1126@gmail.com>
2022-12-03 19:23:56 +0800

    Backup Batch

--- backup.bat
+++ backup.bat
@@ -1,2 +1,2 @@
 rem this script is UNUSED
-xcopy C:\Users\FredB\Desktop\¹¤¾ß\OnlineJudgeFE U:\OnlineJudgeFE_Backup\ /e /y /EXCLUDE:EXCLUDE.txt
+xcopy C:\Users\FredB\Desktop\å·¥å…·\OnlineJudgeFE U:\OnlineJudgeFE_Backup\ /e /y /EXCLUDE:EXCLUDE.txt
