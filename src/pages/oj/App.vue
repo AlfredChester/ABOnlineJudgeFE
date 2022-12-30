2f245cac4072c13e220dc482da92f394ec5b5642
FredB-mine <fredbao1126@gmail.com>
2022-12-30 08:28:55 +0800

    Footer bugfixes

--- src/pages/oj/App.vue
+++ src/pages/oj/App.vue
@@ -9,57 +9,59 @@
     </div>
     <div class="footer">
       <span class="wrapped">
-        <span class="logo">
-          <img src="../../assets/icon_small.png" height="100px"/>
-        </span>
-        <span class="slogan">
-          <div>ABOJ,</div>
-          <div>{{$t('m.OI_Dream_start')}}</div>
-        </span>
-        <span class="infoCard">
-          <p>
-            <span>            
-              <a href="/status">{{$t('m.Judge_Queue')}}</a>
-              |
-              <a href="/about">{{$t('m.Judger')}}</a>
-              |
-              <a href="/FAQ">{{$t('m.FAQ')}}</a>
-              |
-              <a href="/ThanksPage">{{$t('m.Special_Thanks_AppVue')}}</a>
-            </span>
-            <br/>
-            <span>
-              Developed by the ABOJ Dev Team,
-              <a href="mailto:abonlinejudge@163.com">Contact us.</a>
-            </span>
-            <br/>
-            <span>
-              Copyright &copy; 2022-present 
-              <a href="https://github.com/FredB-mine" target="_blank">
-                Dr.Alfred.
-              </a>
-            </span>
-            <br/>
-            <span>
-              Powered by 
-              <a href="https://github.com/QingdaoU/OnlineJudge" target="_blank">
-                OnlineJudge.
-              </a>
-            </span>
-            <br/>
-            <span>
-              <span class="upperBox">
-                <Select 
-                  placeholder="更改语言" style="width: auto;" 
-                  size="small" prefix="md-globe" v-model="langCache">
-                  <Option v-for="(name, _ID) in languages" 
-                    :value="name" :label="_ID" :key="name">
-                  </Option>
-                </Select>
+        <span class="inner_wrap">
+          <span class="logo">
+            <img src="../../assets/icon_small.png" height="100px"/>
+          </span>
+          <span class="slogan">
+            <div>ABOJ,</div>
+            <div>{{$t('m.OI_Dream_start')}}</div>
+          </span>
+          <span class="infoCard">
+            <p>
+              <span>            
+                <a href="/status">{{$t('m.Judge_Queue')}}</a>
+                |
+                <a href="/about">{{$t('m.Judger')}}</a>
+                |
+                <a href="/FAQ">{{$t('m.FAQ')}}</a>
+                |
+                <a href="/ThanksPage">{{$t('m.Special_Thanks_AppVue')}}</a>
               </span>
-              Build Version: {{version}}.
-            </span>
-          </p>
+              <br/>
+              <span>
+                Developed by the ABOJ Dev Team,
+                <a href="mailto:abonlinejudge@163.com">Contact us.</a>
+              </span>
+              <br/>
+              <span>
+                Copyright &copy; 2022-present 
+                <a href="https://github.com/FredB-mine" target="_blank">
+                  Dr.Alfred.
+                </a>
+              </span>
+              <br/>
+              <span>
+                Powered by 
+                <a href="https://github.com/QingdaoU/OnlineJudge" target="_blank">
+                  OnlineJudge.
+                </a>
+              </span>
+              <br/>
+              <span>
+                <span class="upperBox">
+                  <Select 
+                    placeholder="更改语言" style="width: auto;" 
+                    size="small" prefix="md-globe" v-model="langCache">
+                    <Option v-for="(name, _ID) in languages" 
+                      :value="name" :label="_ID" :key="name">
+                    </Option>
+                  </Select>
+                </span>
+                Build Version: {{version}}.
+              </span>
+            </p>
+          </span>
         </span>
       </span>
     </div>
@@ -178,35 +180,37 @@
       margin-right: auto;
       margin-top: 20px;
       margin-bottom: 20px;
-      display: flex;
       color: white;
-      .logo {
-        margin-top: auto;
-        margin-bottom: auto;
-      }
-      .slogan {
-        margin-top: auto;
-        margin-bottom: auto;
-        margin-left: 10px;
-        font-weight: bold;
-        line-height: 1.5;
-        font-size: 1.6em;
-        text-align: left;
-      }
-      .infoCard {
-        flex: 1;
-        text-align: right;
-        min-width: 50vw;
-        margin-top: auto;
-        margin-bottom: auto;
-        .upperBox {
-          margin-right: 20px;
-          .ivu-select {
-            .ivu-select-selection {
-              background-color: unset;
-              border: none;
-              box-shadow: none;
-              color: white;
+      .inner_wrap {
+        min-width: 60vw;
+        display: flex;
+        .logo {
+          margin-top: auto;
+          margin-bottom: auto;
+        }
+        .slogan {
+          margin-top: auto;
+          margin-bottom: auto;
+          margin-left: 10px;
+          font-weight: bold;
+          line-height: 1.5;
+          font-size: 1.6em;
+          text-align: left;
+        }
+        .infoCard {
+          flex: 1;
+          text-align: right;
+          margin-top: auto;
+          margin-bottom: auto;
+          .upperBox {
+            margin-right: 20px;
+            .ivu-select {
+              .ivu-select-selection {
+                background-color: unset;
+                border: none;
+                box-shadow: none;
+                color: white;
+              }
             }
           }
         }
