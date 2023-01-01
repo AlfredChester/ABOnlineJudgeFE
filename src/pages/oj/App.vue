a6742ac210e0e8d82efb88ac5d9695679cb04687
FredB-mine <fredbao1126@gmail.com>
2023-01-01 14:25:17 +0800

    Changed Architecture

--- src/pages/oj/App.vue
+++ src/pages/oj/App.vue
@@ -7,91 +7,20 @@
       </transition>
       <BackTop></BackTop>
     </div>
-    <div class="footer">
-      <span class="wrapped">
-        <span class="inner_wrap">
-          <span class="logo">
-            <img src="../../assets/icon_small.png" height="100px"/>
-          </span>
-          <span class="slogan">
-            <div>ABOJ,</div>
-            <div>{{$t('m.OI_Dream_start')}}</div>
-          </span>
-          <span class="infoCard">
-            <p>
-              <span>            
-                <a href="/status">
-                  {{ $t('m.Judge_Queue') }}
-                </a> |
-                <a href="/about">
-                  {{ $t('m.Judger') }}
-                </a> |
-                <a href="/FAQ">
-                  {{ $t('m.FAQ') }}
-                </a> |
-                <a href="/ThanksPage">
-                  {{ $t('m.Special_Thanks_AppVue') }}
-                </a>
-              </span>
-              <br/>
-              <span>
-                Developed by the ABOJ Dev Team,
-                <a href="mailto:abonlinejudge@163.com">Contact us.</a>
-              </span>
-              <br/>
-              <span>
-                Copyright &copy; 2022-present 
-                <a href="https://github.com/FredB-mine" target="_blank">
-                  Dr.Alfred.
-                </a>
-              </span>
-              <br/>
-              <span>
-                Powered by 
-                <a href="https://github.com/QingdaoU/OnlineJudge" target="_blank">
-                  OnlineJudge.
-                </a>
-              </span>
-              <br/>
-              <span>
-                <span class="upperBox">
-                  <Select 
-                    placeholder="更改语言" style="width: auto;" 
-                    size="small" prefix="md-globe" v-model="langCache">
-                    <Option v-for="(name, _ID) in languages" 
-                      :value="name" :label="_ID" :key="name">
-                    </Option>
-                  </Select>
-                </span>
-                Build Version: {{ version }}.
-              </span>
-            </p>
-          </span>
-        </span>
-      </span>
-    </div>
+    <MyFooter></MyFooter>
   </div>
 </template>
 
 <script>
   import { mapActions, mapState } from 'vuex'
   import NavBar from '@oj/components/NavBar.vue'
+  import MyFooter from '@oj/components/Footer.vue'
 
   export default {
     name: 'app',
     components: {
-      NavBar
-    },
-    data() {
-      return {
-        version: process.env.VERSION,
-        languages: {
-          '简体中文': 'zh-CN',
-          '繁體中文': 'zh-TW',
-          'English': 'en-US'
-        },
-        langCache: this.$i18n.locale
-      }
+      NavBar,
+      MyFooter
     },
     created() {
       try {
@@ -114,13 +43,6 @@
       },
       '$route'() {
         this.changeDomTitle()
-      },
-      'langCache'() {
-        this.$i18n.locale = this.langCache
-        this.$success(this.$i18n.t('m.Succeeded'))
-      },
-      '$i18n.locale'(newVal) {
-        this.langCache = newVal
       }
     }
   }
@@ -174,63 +96,6 @@
 
   .content-app {
     min-height: calc(100vh - 60px);
-  }
-
-  .footer {
-    background-color: rgba(0, 0, 0, .75);
-    margin-top: 20px;
-    text-align: center;
-    font-size: small;
-    display: flex;
-    .wrapped {
-      vertical-align: middle;
-      margin-left: auto;
-      margin-right: auto;
-      margin-top: 20px;
-      margin-bottom: 20px;
-      color: white;
-      .inner_wrap {
-        min-width: 60vw;
-        display: flex;
-        .logo {
-          margin-top: auto;
-          margin-bottom: auto;
-        }
-        .slogan {
-          margin-top: auto;
-          margin-bottom: auto;
-          margin-left: 10px;
-          font-weight: bold;
-          line-height: 1.5;
-          font-size: 1.6em;
-          text-align: left;
-        }
-        .infoCard {
-          flex: 1;
-          text-align: right;
-          margin-top: auto;
-          margin-bottom: auto;
-          .upperBox {
-            margin-right: 20px;
-            .ivu-select {
-              .ivu-select-selection {
-                background-color: unset;
-                border: none;
-                box-shadow: none;
-                color: white;
-              }
-            }
-          }
-        }
-      }
-      a {
-        color: rgba(255, 255, 255, .75);
-        &:active,
-        &:hover {
-          color: white;
-        }
-      }
-    }
   }
 
   .fadeInUp-enter-active {
