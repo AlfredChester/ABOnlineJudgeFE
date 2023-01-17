b45f0932eb6eff5df9b71bd074dadad585c71da6
FredB-mine <fredbao1126@gmail.com>
2023-01-18 07:58:57 +0800

    Changed dates

--- static/js/grey.js
+++ static/js/grey.js
@@ -1,19 +1,17 @@
 // CopyRight © Rhodes Island Phr.Ltd. All Rights Reserverd
-
 'use strict'
 
-const greyDay = [
-  '11-30'
-]
+const greyDay = []
 
 const greyDuration = [
   '2022-11-30~2022-12-6',
-  '2023-1-5~2023-1-5'
+  '2023-1-5~2023-1-5',
+  '2023-1-11~2023-1-14'
 ]
 
-let time = new Date()
+var time = new Date()
 
-let timeLis = time.toLocaleDateString().replace('/', '-').replace('/', '-')
+var timeLis = time.toLocaleDateString().replace('/', '-').replace('/', '-')
 
 function greyAll() {
   $('html').css({
@@ -36,17 +34,12 @@ function matchDuration(src) {
   let beg = Numberlize(fromAndTo[0].split('-'))
   let end = Numberlize(fromAndTo[1].split('-'))
   let now = Numberlize(timeLis.split('-'))
-  // console.log('Beg: ', beg)
-  // console.log('End: ', end)
-  // console.log('Now:', now)
   return beg <= now && now <= end
 }
 
 function matchDay(src) {
   let now = Number(timeLis.split('-')[1]) * 100 + Number(timeLis.split('-')[2])
   let match = Number(src.split('-')[0]) * 100 + Number(src.split('-')[1])
-  // console.log('Now: ', now)
-  // console.log('Match: ', match)
   return now === match
 }
 
