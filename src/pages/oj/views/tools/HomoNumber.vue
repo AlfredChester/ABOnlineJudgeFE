8b3bafc9ee73f18a04043dc228fa4ab13be8718b
FredB-mine <fredbao1126@gmail.com>
2023-02-01 10:39:00 +0800

    HomoNumber Page

---
+++ src/pages/oj/views/tools/HomoNumber.vue
@@ -0,0 +1,15 @@
+<template>
+
+</template>
+
+<script></script>
+
+<style lang="less">
+  .container {
+    margin-bottom: 20px;
+    .content {
+      font-size: 16px;
+      margin: 0 50px 20px 50px;
+    }
+  }
+</style>
