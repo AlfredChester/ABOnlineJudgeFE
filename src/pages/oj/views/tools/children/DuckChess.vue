dc0301bd56cca833c023723438b11f417c70654d
FredB-mine <fredbao1126@gmail.com>
2023-06-28 16:57:09 +0800

    Big Update

---
+++ src/pages/oj/views/tools/children/DuckChess.vue
@@ -0,0 +1,13 @@
+<template>
+  <iframe src="/static/games/DuckchessOnline/index.html"
+          class="DuckChessFrame">
+  </iframe>
+</template>
+
+<style lang="less">
+  .DuckChessFrame {
+    width: 100%;
+    height: 80vh;
+    border: none;
+  }
+</style>
