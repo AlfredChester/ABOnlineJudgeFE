b54b8807aaf1c2a15ca3662318a7105dd9197761
FredB-mine <fredbao1126@gmail.com>
2023-06-28 16:57:12 +0800

    Big Update

---
+++ src/pages/oj/views/tools/index.js
@@ -0,0 +1,11 @@
+const Tools = () => import(/* webpackChunkName: "tools" */ './Tools.vue')
+const CodeCompresser = () => import(/* webpackChunkName: "tools" */ './children/CodeCompresser.vue')
+const DownloadPage = () => import(/* webpackChunkName: "tools" */ './children/DownloadPage.vue')
+const DuckChess = () => import(/* webpackChunkName: "tools" */ './children/DuckChess.vue')
+const HomoNumber = () => import(/* webpackChunkName: "tools" */ './children/HomoNumber.vue')
+const Listening = () => import(/* webpackChunkName: "tools" */ './children/Listening.vue')
+
+export {
+    Tools, CodeCompresser, DownloadPage,
+    DuckChess, HomoNumber, Listening
+}
