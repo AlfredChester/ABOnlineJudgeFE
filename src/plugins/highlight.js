d9741a3c8cd659e22878b7186c3d589a3fe59fea
FredB-mine <fredbao1126@gmail.com>
2023-07-22 11:59:03 +0800

    hljs

--- src/plugins/highlight.js
+++ src/plugins/highlight.js
@@ -1,15 +1,19 @@
-import hljs from 'highlight.js/lib/highlight'
+import hljs from 'highlight.js/lib/core'
 import cpp from 'highlight.js/lib/languages/cpp'
+import go from 'highlight.js/lib/languages/go'
 import python from 'highlight.js/lib/languages/python'
 import java from 'highlight.js/lib/languages/java'
+import js from 'highlight.js/lib/languages/javascript'
 import 'highlight.js/styles/atom-one-light.css'
 
+hljs.registerLanguage('go', go)
 hljs.registerLanguage('cpp', cpp)
 hljs.registerLanguage('java', java)
 hljs.registerLanguage('python', python)
+hljs.registerLanguage('javascript', js)
 
 export default {
-  install (Vue, options) {
+  install(Vue, options) {
     Vue.directive('highlight', {
       deep: true,
       bind: function (el, binding) {
@@ -20,7 +24,7 @@ export default {
           if (binding.value) {
             target.textContent = binding.value
           }
-          hljs.highlightBlock(target)
+          hljs.highlightElement(target)
         })
       },
       componentUpdated: function (el, binding) {
