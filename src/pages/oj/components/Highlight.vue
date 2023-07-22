0615916b2b3425862798971757f429b876c4b08b
FredB-mine <fredbao1126@gmail.com>
2023-07-22 11:58:49 +0800

    Highlight

--- src/pages/oj/components/Highlight.vue
+++ src/pages/oj/components/Highlight.vue
@@ -1,47 +1,51 @@
 <template>
-  <pre v-highlight="code">
-    <code :class="language" :style="styleObject"></code>
-  </pre>
+  <pre v-highlight="code"><code :class="language" :style="styleObject"></code></pre>
 </template>
 
 <script>
-  export default {
-    name: 'highlight',
-    data () {
-      return {
-        styleObject: {
-          'border-left': '2px solid green'
-        }
+export default {
+  name: 'highlight',
+  data() {
+    return {
+      styleObject: {
+        'border-left': 'none'
       }
+    }
+  },
+  props: {
+    language: {
+      type: String
     },
-    props: {
-      language: {
-        type: String
-      },
-      code: {
-        required: true,
-        type: String
-      },
-      borderColor: {
-        type: String,
-        default: 'green'
-      }
+    code: {
+      required: true,
+      type: String
     },
-    watch: {
-      'borderColor' (newVal, oldVal) {
+    borderColor: {
+      type: String,
+      default: 'none'
+    }
+  },
+  watch: {
+    'borderColor'(newVal, oldVal) {
+      console.log(newVal)
+      if (newVal === 'none') {
+        this.styleObject = {}
+      } else {
         this.styleObject['border-left'] = '2.5px solid ' + newVal
       }
     }
   }
+}
 </script>
 
 <style scoped lang="less">
-  pre {
-    padding: 0;
-    display: block;
-    code {
-      padding: 20px;
-      font-size: 1.1em;
-    }
+pre {
+  padding: 0;
+  // display: block;
+
+  code {
+    padding: 20px;
+    font-size: 1.1em;
   }
+}
 </style>
