f0d581be40f0ba41ef9fa0a8d3a0003ff2a87c34
FredB-mine <fredbao1126@gmail.com>
2023-07-23 11:04:43 +0800

    Problem links(unfinished

---
+++ src/pages/oj/components/ProblemLink.vue
@@ -0,0 +1,54 @@
+<template>
+    <a :href="'/problem/'">
+      
+    </a>
+  </template>
+  
+<script>
+import api from '@oj/api'
+
+export default {
+  name: 'UserLink',
+  data() {
+    return {
+      loading: true,
+      data: undefined
+    }
+  },
+  mounted() {
+    this.reload()
+  },
+  props: {
+    problemId: {
+      required: true,
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    reload() {
+      if (this.problemId !== undefined && this.problemId !== '') {
+        api.getProblemByID(this.problemId).then(res => {
+          console.log(res.data.data)
+        })
+      }
+    }
+  },
+  watch: {
+    'problemId'(newVal, oldVal) {
+      this.reload()
+    }
+  }
+}
+</script>
+  
+<style lang="less">
+a {
+  color: #495060;
+
+  &:hover {
+    color: #2d8cf0;
+    border-bottom: none;
+  }
+}
+</style>
