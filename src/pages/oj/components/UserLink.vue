ab33a818a67fb57b8bda303152de474a8a0f9209
FredB-mine <fredbao1126@gmail.com>
2023-07-23 11:04:47 +0800

    User links

---
+++ src/pages/oj/components/UserLink.vue
@@ -0,0 +1,60 @@
+<template>
+  <a :href="'/user-home?username=' + username">
+    <span v-if="!loading">
+      <Avatar :src="image"></Avatar>
+    </span>
+    {{ username }}
+  </a>
+</template>
+
+<script>
+import api from '@oj/api'
+
+export default {
+  name: 'UserLink',
+  data() {
+    return {
+      loading: true,
+      image: undefined
+    }
+  },
+  mounted() {
+    this.reload()
+  },
+  props: {
+    username: {
+      required: true,
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    reload() {
+      if (this.username !== '') {
+        api.getUserInfo(this.username).then(res => {
+          this.loading = false
+          this.image = res.data.data.avatar
+        }).catch(err => {
+          console.log('[oj/UserLink.vue]: Error', err)
+        })
+      }
+    }
+  },
+  watch: {
+    'username'(newVal, oldVal) {
+      this.reload()
+    }
+  }
+}
+</script>
+
+<style lang="less">
+a {
+  color: #495060;
+
+  &:hover {
+    color: #2d8cf0;
+    border-bottom: none;
+  }
+}
+</style>
