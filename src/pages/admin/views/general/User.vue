fb34238306ee671a59499e437d6c6e2187732a75
FredB-mine <fredbao1126@gmail.com>
2023-08-07 15:09:13 +0800

    Tag

--- src/pages/admin/views/general/User.vue
+++ src/pages/admin/views/general/User.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="view">
-    <Panel :title="$t('m.User_User') ">
+    <Panel :title="$t('m.User_User')">
       <div slot="header">
         <el-row :gutter="20">
           <el-col :span="8">
             <el-button v-show="selectedUsers.length"
                        type="warning" icon="el-icon-fa-trash"
-                       @click="deleteUsers(selectedUserIDs)">{{$t('m.Delete')}}
+                       @click="deleteUsers(selectedUserIDs)">{{ $t('m.Delete') }}
             </el-button>
           </el-col>
-          <el-col :span="selectedUsers.length ? 16: 24">
+          <el-col :span="selectedUsers.length ? 16 : 24">
             <el-input v-model="keyword" prefix-icon="el-icon-search" :placeholder="$t('m.Keywords')"></el-input>
           </el-col>
         </el-row>
@@ -29,13 +29,13 @@
 
         <el-table-column prop="create_time" :label="$t('m.Create_Time')">
           <template slot-scope="scope">
-            {{scope.row.create_time | localtime }}
+            {{ scope.row.create_time | localtime }}
           </template>
         </el-table-column>
 
         <el-table-column prop="last_login" :label="$t('m.Last_Login')">
           <template slot-scope="scope">
-            {{scope.row.last_login | localtime }}
+            {{ scope.row.last_login | localtime }}
           </template>
         </el-table-column>
 
@@ -68,10 +68,10 @@
     </Panel>
 
     <Panel>
-      <span slot="title">{{$t('m.Import_User')}}
+      <span slot="title">{{ $t('m.Import_User') }}
         <el-popover placement="right" trigger="hover">
-          <p>{{$t('m.Import_User_Help_1')}}<a
-            href="http://docs.onlinejudge.me/#/onlinejudge/guide/import_users">{{$t('m.Import_User_Help_2')}}</a>{{$t('m.Import_User_Help_3')}}</p>
+          <p>{{ $t('m.Import_User_Help_1') }}<a
+            href="http://docs.onlinejudge.me/#/onlinejudge/guide/import_users">{{ $t('m.Import_User_Help_2') }}</a>{{ $t('m.Import_User_Help_3') }}</p>
           <i slot="reference" class="el-icon-fa-question-circle import-user-icon"></i>
         </el-popover>
       </span>
@@ -80,39 +80,39 @@
                  :show-file-list="false"
                  accept=".csv"
                  :before-upload="handleUsersCSV">
-        <el-button size="small" icon="el-icon-fa-upload" type="primary">{{$t('m.Choose_File')}}</el-button>
+        <el-button size="small" icon="el-icon-fa-upload" type="primary">{{ $t('m.Choose_File') }}</el-button>
       </el-upload>
       <template v-else>
         <el-table :data="uploadUsersPage">
           <el-table-column :label="$t('m.username')">
             <template slot-scope="{row}">
-              {{row[0]}}
+              {{ row[0] }}
             </template>
           </el-table-column>
           <el-table-column :label="$t('m.Password')">
             <template slot-scope="{row}">
-              {{row[1]}}
+              {{ row[1] }}
             </template>
           </el-table-column>
           <el-table-column :label="$t('m.Email')">
             <template slot-scope="{row}">
-              {{row[2]}}
+              {{ row[2] }}
             </template>
           </el-table-column>
           <el-table-column :label="$t('m.User_Real_Name')">
             <template slot-scope="{row}">
-              {{row[3]}}
+              {{ row[3] }}
             </template>
           </el-table-column>
         </el-table>
         <div class="panel-options">
           <el-button type="primary" size="small"
                      icon="el-icon-fa-upload"
-                     @click="handleUsersUpload">{{$t('m.Import_All_User')}}
+                     @click="handleUsersUpload">{{ $t('m.Import_All_User') }}
           </el-button>
           <el-button type="warning" size="small"
                      icon="el-icon-fa-undo"
-                     @click="handleResetData">{{$t('m.Reset_Data')}}
+                     @click="handleResetData">{{ $t('m.Reset_Data') }}
           </el-button>
           <el-pagination
             class="page"
@@ -157,16 +157,16 @@
         </el-row>
 
         <el-form-item>
-          <el-button type="primary" @click="generateUser" icon="el-icon-fa-users" :loading="loadingGenerate">{{$t('m.Gen_and_export')}}
+          <el-button type="primary" @click="generateUser" icon="el-icon-fa-users" :loading="loadingGenerate">{{ $t('m.Gen_and_export') }}
           </el-button>
           <span class="userPreview" v-if="formGenerateUser.number_from && formGenerateUser.number_to &&
-                                          formGenerateUser.number_from <= formGenerateUser.number_to">
-            {{$t('m.UserNames_Will_Be')}} {{formGenerateUser.prefix + formGenerateUser.number_from + formGenerateUser.suffix}},
+            formGenerateUser.number_from <= formGenerateUser.number_to">
+            {{ $t('m.UserNames_Will_Be') }} {{ formGenerateUser.prefix + formGenerateUser.number_from + formGenerateUser.suffix }},
             <span v-if="formGenerateUser.number_from + 1 < formGenerateUser.number_to">
-              {{formGenerateUser.prefix + (formGenerateUser.number_from + 1) + formGenerateUser.suffix + '...'}}
+              {{ formGenerateUser.prefix + (formGenerateUser.number_from + 1) + formGenerateUser.suffix + '...' }}
             </span>
             <span v-if="formGenerateUser.number_from + 1 <= formGenerateUser.number_to">
-              {{formGenerateUser.prefix + formGenerateUser.number_to + formGenerateUser.suffix}}
+              {{ formGenerateUser.prefix + formGenerateUser.number_to + formGenerateUser.suffix }}
             </span>
           </span>
         </el-form-item>
@@ -207,7 +207,7 @@
           </el-col>
           <el-col :span="12">
             <el-form-item :label="$t('m.Problem_Permission')">
-              <el-select v-model="user.problem_permission" :disabled="user.admin_type!=='Admin'">
+              <el-select v-model="user.problem_permission" :disabled="user.admin_type !== 'Admin'">
                 <el-option :label="$t('m.No_Question_Authority')" value="None"></el-option>
                 <el-option :label="$t('m.Own_Question_Authority')" value="Own"></el-option>
                 <el-option :label="$t('m.All_Question_Authority')" value="All"></el-option>
@@ -243,7 +243,7 @@
         </el-row>
       </el-form>
       <span slot="footer" class="dialog-footer">
-        <cancel @click.native="showUserDialog = false">{{$t('m.Cancel_i18n')}}</cancel>
+        <cancel @click.native="showUserDialog = false">{{ $t('m.Cancel_i18n') }}</cancel>
         <save @click.native="saveUser()"></save>
       </span>
     </el-dialog>
@@ -251,190 +251,190 @@
 </template>
 
 <script>
-  import papa from 'papaparse'
-  import api from '../../api.js'
-  import utils from '@/utils/utils'
+import papa from 'papaparse'
+import api from '../../api.js'
+import utils from '@/utils/utils'
 
-  export default {
-    name: 'User',
-    data () {
-      return {
-        // 一页显示的用户数
-        pageSize: 10,
-        // 用户总数
-        total: 0,
-        // 用户列表
-        userList: [],
-        uploadUsers: [],
-        uploadUsersPage: [],
-        uploadUsersCurrentPage: 1,
-        uploadUsersPageSize: 15,
-        // 搜索关键字
-        keyword: '',
-        // 是否显示用户对话框
-        showUserDialog: false,
-        // 当前用户model
-        user: {},
-        loadingTable: false,
-        loadingGenerate: false,
-        // 当前页码
-        currentPage: 0,
-        selectedUsers: [],
-        formGenerateUser: {
-          prefix: '',
-          suffix: '',
-          number_from: 0,
-          number_to: 0,
-          password_length: 8
-        }
-      }
-    },
-    mounted () {
-      this.getUserList(1)
-    },
-    methods: {
-      // 切换页码回调
-      currentChange (page) {
-        this.currentPage = page
-        this.getUserList(page)
-      },
-      // 提交修改用户的信息
-      saveUser () {
-        api.editUser(this.user).then(res => {
-          // 更新列表
-          this.getUserList(this.currentPage)
-        }).then(() => {
-          this.showUserDialog = false
-        }).catch(() => {
-        })
-      },
-      // 打开用户对话框
-      openUserDialog (id) {
-        this.showUserDialog = true
-        // console.log('id:', id)
-        api.getUser(id).then(res => {
-          this.user = res.data.data
-          this.user.password = ''
-          this.user.real_tfa = this.user.two_factor_auth
-        })
-      },
-      // 获取用户列表
-      getUserList (page) {
-        this.loadingTable = true
-        api.getUserList((page - 1) * this.pageSize, this.pageSize, this.keyword).then(res => {
-          this.loadingTable = false
-          this.total = res.data.data.total
-          this.userList = res.data.data.results
-        }, res => {
-          this.loadingTable = false
-        })
-      },
-      deleteUsers (ids) {
-        this.$confirm('Sure to delete the user? The associated resources created by this user will be deleted as well, like problem, contest, announcement, etc.', 'confirm', {
-          type: 'warning'
-        }).then(() => {
-          api.deleteUsers(ids.join(',')).then(res => {
-            this.getUserList(this.currentPage)
-          }).catch(() => {
-            this.getUserList(this.currentPage)
-          })
-        }, () => {
-        })
-      },
-      handleSelectionChange (val) {
-        this.selectedUsers = val
-      },
-      generateUser () {
-        this.$refs['formGenerateUser'].validate((valid) => {
-          if (!valid) {
-            this.$error('Please validate the error fields')
-            return
-          }
-          this.loadingGenerate = true
-          let data = Object.assign({}, this.formGenerateUser)
-          api.generateUser(data).then(res => {
-            this.loadingGenerate = false
-            let url = '/admin/generate_user?file_id=' + res.data.data.file_id
-            utils.downloadFile(url).then(() => {
-              this.$alert('All users created successfully, the users sheets have downloaded to your disk.', 'Notice')
-            })
-            this.getUserList(1)
-          }).catch(() => {
-            this.loadingGenerate = false
-          })
-        })
-      },
-      handleUsersCSV (file) {
-        papa.parse(file, {
-          complete: (results) => {
-            let data = results.data.filter(user => {
-              return user[0] && user[1] && user[2] && user[3]
-            })
-            let delta = results.data.length - data.length
-            if (delta > 0) {
-              this.$warning(delta + ' users have been filtered due to empty value')
-            }
-            this.uploadUsersCurrentPage = 1
-            this.uploadUsers = data
-            this.uploadUsersPage = data.slice(0, this.uploadUsersPageSize)
-          },
-          error: (error) => {
-            this.$error(error)
-          }
-        })
-      },
-      handleUsersUpload () {
-        api.importUsers(this.uploadUsers).then(res => {
-          this.getUserList(1)
-          this.handleResetData()
-        }).catch(() => {
-        })
-      },
-      handleResetData () {
-        this.uploadUsers = []
-      }
-    },
-    computed: {
-      selectedUserIDs () {
-        let ids = []
-        for (let user of this.selectedUsers) {
-          ids.push(user.id)
-        }
-        return ids
-      }
-    },
-    watch: {
-      'keyword' () {
-        this.currentChange(1)
-      },
-      'user.admin_type' () {
-        if (this.user.admin_type === 'Super Admin') {
-          this.user.problem_permission = 'All'
-        } else if (this.user.admin_type === 'Regular User') {
-          this.user.problem_permission = 'None'
-        }
-      },
-      'uploadUsersCurrentPage' (page) {
-        this.uploadUsersPage = this.uploadUsers.slice((page - 1) * this.uploadUsersPageSize, page * this.uploadUsersPageSize)
+export default {
+  name: 'User',
+  data() {
+    return {
+      // 一页显示的用户数
+      pageSize: 10,
+      // 用户总数
+      total: 0,
+      // 用户列表
+      userList: [],
+      uploadUsers: [],
+      uploadUsersPage: [],
+      uploadUsersCurrentPage: 1,
+      uploadUsersPageSize: 15,
+      // 搜索关键字
+      keyword: '',
+      // 是否显示用户对话框
+      showUserDialog: false,
+      // 当前用户model
+      user: {},
+      loadingTable: false,
+      loadingGenerate: false,
+      // 当前页码
+      currentPage: 0,
+      selectedUsers: [],
+      formGenerateUser: {
+        prefix: '',
+        suffix: '',
+        number_from: 0,
+        number_to: 0,
+        password_length: 8
       }
     }
+  },
+  mounted() {
+    this.getUserList(1)
+  },
+  methods: {
+    // 切换页码回调
+    currentChange(page) {
+      this.currentPage = page
+      this.getUserList(page)
+    },
+    // 提交修改用户的信息
+    saveUser() {
+      api.editUser(this.user).then(res => {
+        // 更新列表
+        this.getUserList(this.currentPage)
+      }).then(() => {
+        this.showUserDialog = false
+      }).catch(() => {
+      })
+    },
+    // 打开用户对话框
+    openUserDialog(id) {
+      this.showUserDialog = true
+      api.getUser(id).then(res => {
+        this.user = res.data.data
+        console.log(res.data.data)
+        this.user.password = ''
+        this.user.real_tfa = this.user.two_factor_auth
+      })
+    },
+    // 获取用户列表
+    getUserList(page) {
+      this.loadingTable = true
+      api.getUserList((page - 1) * this.pageSize, this.pageSize, this.keyword).then(res => {
+        this.loadingTable = false
+        this.total = res.data.data.total
+        this.userList = res.data.data.results
+      }, res => {
+        this.loadingTable = false
+      })
+    },
+    deleteUsers(ids) {
+      this.$confirm('Sure to delete the user? The associated resources created by this user will be deleted as well, like problem, contest, announcement, etc.', 'confirm', {
+        type: 'warning'
+      }).then(() => {
+        api.deleteUsers(ids.join(',')).then(res => {
+          this.getUserList(this.currentPage)
+        }).catch(() => {
+          this.getUserList(this.currentPage)
+        })
+      }, () => {
+      })
+    },
+    handleSelectionChange(val) {
+      this.selectedUsers = val
+    },
+    generateUser() {
+      this.$refs['formGenerateUser'].validate((valid) => {
+        if (!valid) {
+          this.$error('Please validate the error fields')
+          return
+        }
+        this.loadingGenerate = true
+        let data = Object.assign({}, this.formGenerateUser)
+        api.generateUser(data).then(res => {
+          this.loadingGenerate = false
+          let url = '/admin/generate_user?file_id=' + res.data.data.file_id
+          utils.downloadFile(url).then(() => {
+            this.$alert('All users created successfully, the users sheets have downloaded to your disk.', 'Notice')
+          })
+          this.getUserList(1)
+        }).catch(() => {
+          this.loadingGenerate = false
+        })
+      })
+    },
+    handleUsersCSV(file) {
+      papa.parse(file, {
+        complete: (results) => {
+          let data = results.data.filter(user => {
+            return user[0] && user[1] && user[2] && user[3]
+          })
+          let delta = results.data.length - data.length
+          if (delta > 0) {
+            this.$warning(delta + ' users have been filtered due to empty value')
+          }
+          this.uploadUsersCurrentPage = 1
+          this.uploadUsers = data
+          this.uploadUsersPage = data.slice(0, this.uploadUsersPageSize)
+        },
+        error: (error) => {
+          this.$error(error)
+        }
+      })
+    },
+    handleUsersUpload() {
+      api.importUsers(this.uploadUsers).then(res => {
+        this.getUserList(1)
+        this.handleResetData()
+      }).catch(() => {
+      })
+    },
+    handleResetData() {
+      this.uploadUsers = []
+    }
+  },
+  computed: {
+    selectedUserIDs() {
+      let ids = []
+      for (let user of this.selectedUsers) {
+        ids.push(user.id)
+      }
+      return ids
+    }
+  },
+  watch: {
+    'keyword'() {
+      this.currentChange(1)
+    },
+    'user.admin_type'() {
+      if (this.user.admin_type === 'Super Admin') {
+        this.user.problem_permission = 'All'
+      } else if (this.user.admin_type === 'Regular User') {
+        this.user.problem_permission = 'None'
+      }
+    },
+    'uploadUsersCurrentPage'(page) {
+      this.uploadUsersPage = this.uploadUsers.slice((page - 1) * this.uploadUsersPageSize, page * this.uploadUsersPageSize)
+    }
   }
+}
 </script>
 
 <style scoped lang="less">
-  .import-user-icon {
-    color: #555555;
-    margin-left: 4px;
-  }
+.import-user-icon {
+  color: #555555;
+  margin-left: 4px;
+}
 
-  .userPreview {
-    padding-left: 10px;
-  }
+.userPreview {
+  padding-left: 10px;
+}
 
-  .notification {
-    p {
-      margin: 0;
-      text-align: left;
-    }
+.notification {
+  p {
+    margin: 0;
+    text-align: left;
   }
+}
 </style>
