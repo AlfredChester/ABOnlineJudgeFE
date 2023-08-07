938bca5a30bb83437216b710ea8fff2b4de2b229
FredB-mine <fredbao1126@gmail.com>
2023-08-07 15:09:15 +0800

    Tag

--- src/pages/oj/views/problem/ProblemList.vue
+++ src/pages/oj/views/problem/ProblemList.vue
@@ -2,26 +2,26 @@
   <Row type="flex" :gutter="18">
     <Col :span="19">
     <Panel shadow>
-      <div slot="title">{{$t('m.Problem_List')}}</div>
+      <div slot="title">{{ $t('m.Problem_List') }}</div>
       <div slot="extra">
         <ul class="filter">
           <li>
             <Dropdown @on-click="filterByDifficulty">
-              <span>{{query.difficulty === '' ? this.$i18n.t('m.Difficulty') : this.$i18n.t('m.' + query.difficulty)}}
+              <span>{{ query.difficulty === '' ? this.$i18n.t('m.Difficulty') : this.$i18n.t('m.' + query.difficulty) }}
                 <Icon type="md-arrow-dropdown"></Icon>
               </span>
               <Dropdown-menu slot="list">
-                <Dropdown-item name="">{{$t('m.All')}}</Dropdown-item>
-                <Dropdown-item name="Low">{{$t('m.Low')}}</Dropdown-item>
-                <Dropdown-item name="Mid" >{{$t('m.Mid')}}</Dropdown-item>
-                <Dropdown-item name="High">{{$t('m.High')}}</Dropdown-item>
+                <Dropdown-item name="">{{ $t('m.All') }}</Dropdown-item>
+                <Dropdown-item name="Low">{{ $t('m.Low') }}</Dropdown-item>
+                <Dropdown-item name="Mid" >{{ $t('m.Mid') }}</Dropdown-item>
+                <Dropdown-item name="High">{{ $t('m.High') }}</Dropdown-item>
               </Dropdown-menu>
             </Dropdown>
           </li>
           <li>
             <i-switch size="large" @on-change="handleTagsVisible">
-              <span slot="open">{{$t('m.Tags')}}</span>
-              <span slot="close">{{$t('m.Tags')}}</span>
+              <span slot="open">{{ $t('m.Tags') }}</span>
+              <span slot="close">{{ $t('m.Tags') }}</span>
             </i-switch>
           </li>
           <li>
@@ -38,7 +38,7 @@
           <li>
             <Button type="info" @click="onReset">
               <Icon type="md-refresh" size="15"></Icon>
-              {{$t('m.Reset')}}
+              {{ $t('m.Reset') }}
             </Button>
           </li>
         </ul>
@@ -56,16 +56,16 @@
 
     <Col :span="5">
       <Card :padding="10" style="margin-bottom: 10px;">
-        <div class="saying_title">{{$t('m.hitokoto')}}</div>
+        <div class="saying_title">{{ $t('m.hitokoto') }}</div>
         <div class="saying_content">
-          {{saying}}
+          {{ saying }}
         </div>
         <div class="saying_content saying_src" v-if="saying_src">
-          ——{{saying_src}}
+          ——{{ saying_src }}
         </div>
       </Card>
       <Panel :padding="10">
-        <div slot="title" class="taglist-title">{{$t('m.Tags')}}</div>
+        <div slot="title" class="taglist-title">{{ $t('m.Tags') }}</div>
         <Scroll>
           <Button v-for="tag in tagList" 
                   :key="tag.name"
@@ -79,7 +79,7 @@
 
         <Button long id="pick-one" @click="pickone">
           <Icon type="ios-shuffle" size="15"></Icon>
-          {{$t('m.Pick_One')}}
+          {{ $t('m.Pick_One') }}
         </Button>
       </Panel>
       <Spin v-if="loadings.tag" fix size="large"></Spin>
@@ -88,277 +88,277 @@
 </template>
 
 <script>
-  import { mapGetters } from 'vuex'
-  import api from '@oj/api'
-  import utils from '@/utils/utils'
-  import { ProblemMixin } from '@oj/components/mixins'
-  import Pagination from '@oj/components/Pagination'
+import { mapGetters } from 'vuex'
+import api from '@oj/api'
+import utils from '@/utils/utils'
+import { ProblemMixin } from '@oj/components/mixins'
+import Pagination from '@oj/components/Pagination'
 
-  // Add Params Here
-  const params = '?c=a&c=c&c=d&c=i&c=k'
+// Add Params Here
+const params = '?c=a&c=c&c=d&c=i&c=k'
 
-  const axios = require('axios')
+const axios = require('axios')
 
-  export default {
-    name: 'ProblemList',
-    mixins: [ProblemMixin],
-    components: {
-      Pagination
-    },
-    data () {
-      return {
-        tagList: [],
-        problemTableColumns: [
-          {
-            title: this.$i18n.t('m.Problem_ID'),
-            key: '_id',
-            width: 80,
-            render: (h, params) => {
-              return h('Button', {
-                props: {
-                  type: 'text',
-                  size: 'large'
-                },
-                on: {
-                  click: () => {
-                    this.$router.push({name: 'problem-details', params: {problemID: params.row._id}})
-                  }
-                },
-                style: {
-                  padding: '2px 0'
+export default {
+  name: 'ProblemList',
+  mixins: [ProblemMixin],
+  components: {
+    Pagination
+  },
+  data() {
+    return {
+      tagList: [],
+      problemTableColumns: [
+        {
+          title: this.$i18n.t('m.Problem_ID'),
+          key: '_id',
+          width: 80,
+          render: (h, params) => {
+            return h('Button', {
+              props: {
+                type: 'text',
+                size: 'large'
+              },
+              on: {
+                click: () => {
+                  this.$router.push({ name: 'problem-details', params: { problemID: params.row._id } })
                 }
-              }, params.row._id)
-            }
-          },
-          {
-            title: this.$i18n.t('m.Title'),
-            width: 400,
-            render: (h, params) => {
-              return h('Button', {
-                props: {
-                  type: 'text',
-                  size: 'large'
-                },
-                on: {
-                  click: () => {
-                    this.$router.push({name: 'problem-details', params: {problemID: params.row._id}})
-                  }
-                },
-                style: {
-                  padding: '2px 0',
-                  overflowX: 'auto',
-                  textAlign: 'left',
-                  width: '100%'
-                }
-              }, params.row.title)
-            }
-          },
-          {
-            title: this.$i18n.t('m.Level'),
-            render: (h, params) => {
-              let t = params.row.difficulty
-              let color = 'primary'
-              if (t === 'Low') color = 'success'
-              else if (t === 'High') color = 'error'
-              return h('Tag', {
-                props: {
-                  color: color
-                }
-              }, this.$i18n.t('m.' + params.row.difficulty))
-            }
-          },
-          {
-            title: this.$i18n.t('m.Total'),
-            key: 'submission_number'
-          },
-          {
-            title: this.$i18n.t('m.AC_Rate'),
-            render: (h, params) => {
-              return h('span', this.getACRate(params.row.accepted_number, params.row.submission_number))
-            }
-          }
-        ],
-        problemList: [],
-        limit: 20,
-        total: 0,
-        loadings: {
-          table: true,
-          tag: true
-        },
-        routeName: '',
-        query: {
-          keyword: '',
-          difficulty: '',
-          tag: '',
-          page: 1,
-          limit: 10
-        },
-        saying: null,
-        saying_src: null
-      }
-    },
-    mounted () {
-      this.init()
-    },
-    methods: {
-      init (simulate = false) {
-        axios.get(
-          'https://v1.hitokoto.cn/' + params
-        ).then(res => {
-          this.saying = res.data.hitokoto
-          this.saying_src = res.data.from
-        }).catch(err => {
-          console.log('Error when getting hitokoto', err)
-          this.saying = '获取一言失败'
-        })
-        this.routeName = this.$route.name
-        let query = this.$route.query
-        this.query.difficulty = query.difficulty || ''
-        this.query.keyword = query.keyword || ''
-        this.query.tag = query.tag || ''
-        this.query.page = parseInt(query.page) || 1
-        if (this.query.page < 1) {
-          this.query.page = 1
-        }
-        this.query.limit = parseInt(query.limit) || 10
-        if (!simulate) {
-          this.getTagList()
-        }
-        this.getProblemList()
-      },
-      pushRouter () {
-        this.$router.push({
-          name: 'problem-list',
-          query: utils.filterEmptyValue(this.query)
-        })
-      },
-      getProblemList () {
-        let offset = (this.query.page - 1) * this.query.limit
-        this.loadings.table = true
-        api.getProblemList(offset, this.limit, this.query).then(res => {
-          this.loadings.table = false
-          this.total = res.data.data.total
-          this.problemList = res.data.data.results.sort((a, b) => {
-            return a._id < b._id ? -114 : 514
-          })
-          if (this.isAuthenticated) {
-            this.addStatusColumn(this.problemTableColumns, res.data.data.results)
-          }
-        }, res => {
-          this.loadings.table = false
-        })
-      },
-      getTagList () {
-        api.getProblemTagList().then(res => {
-          this.tagList = res.data.data
-          this.loadings.tag = false
-        }, res => {
-          this.loadings.tag = false
-        })
-      },
-      filterByTag (tagName) {
-        if (this.query.tag !== tagName) {
-          this.query.tag = tagName
-          this.query.page = 1
-          this.pushRouter()
-        } else {
-          this.query.tag = ''
-          this.pushRouter()
-        }
-      },
-      filterByDifficulty (difficulty) {
-        this.query.difficulty = difficulty
-        this.query.page = 1
-        this.pushRouter()
-      },
-      filterByKeyword () {
-        this.query.page = 1
-        this.pushRouter()
-      },
-      handleTagsVisible (value) {
-        if (value) {
-          this.problemTableColumns.push(
-            {
-              title: this.$i18n.t('m.Tags'),
-              align: 'center',
-              render: (h, params) => {
-                let tags = []
-                params.row.tags.forEach(tag => {
-                  tags.push(h('Tag', {
-                    on: {
-                      click: () => {
-                        console.log('114514')
-                      }
-                    }
-                  }, tag))
-                })
-                return h('div', {
-                  style: {
-                    margin: '8px 0'
-                  }
-                }, tags)
+              },
+              style: {
+                padding: '2px 0'
               }
-            })
-        } else {
-          this.problemTableColumns.splice(this.problemTableColumns.length - 1, 1)
+            }, params.row._id)
+          }
+        },
+        {
+          title: this.$i18n.t('m.Title'),
+          width: 400,
+          render: (h, params) => {
+            return h('Button', {
+              props: {
+                type: 'text',
+                size: 'large'
+              },
+              on: {
+                click: () => {
+                  this.$router.push({ name: 'problem-details', params: { problemID: params.row._id } })
+                }
+              },
+              style: {
+                padding: '2px 0',
+                overflowX: 'auto',
+                textAlign: 'left',
+                width: '100%'
+              }
+            }, params.row.title)
+          }
+        },
+        {
+          title: this.$i18n.t('m.Level'),
+          render: (h, params) => {
+            let t = params.row.difficulty
+            let color = 'primary'
+            if (t === 'Low') color = 'success'
+            else if (t === 'High') color = 'error'
+            return h('Tag', {
+              props: {
+                color: color
+              }
+            }, this.$i18n.t('m.' + params.row.difficulty))
+          }
+        },
+        {
+          title: this.$i18n.t('m.Total'),
+          key: 'submission_number'
+        },
+        {
+          title: this.$i18n.t('m.AC_Rate'),
+          render: (h, params) => {
+            return h('span', this.getACRate(params.row.accepted_number, params.row.submission_number))
+          }
         }
+      ],
+      problemList: [],
+      limit: 20,
+      total: 0,
+      loadings: {
+        table: true,
+        tag: true
       },
-      onReset () {
-        this.$router.push({name: 'problem-list'})
+      routeName: '',
+      query: {
+        keyword: '',
+        difficulty: '',
+        tag: '',
+        page: 1,
+        limit: 10
       },
-      pickone () {
-        api.pickone().then(res => {
-          this.$success(this.$i18n.t('m.Good_Luck'))
-          this.$router.push({name: 'problem-details', params: {problemID: res.data.data}})
+      saying: null,
+      saying_src: null
+    }
+  },
+  mounted() {
+    this.init()
+  },
+  methods: {
+    init(simulate = false) {
+      axios.get(
+        'https://v1.hitokoto.cn/' + params
+      ).then(res => {
+        this.saying = res.data.hitokoto
+        this.saying_src = res.data.from
+      }).catch(err => {
+        console.log('Error when getting hitokoto', err)
+        this.saying = '获取一言失败'
+      })
+      this.routeName = this.$route.name
+      let query = this.$route.query
+      this.query.difficulty = query.difficulty || ''
+      this.query.keyword = query.keyword || ''
+      this.query.tag = query.tag || ''
+      this.query.page = parseInt(query.page) || 1
+      if (this.query.page < 1) {
+        this.query.page = 1
+      }
+      this.query.limit = parseInt(query.limit) || 10
+      if (!simulate) {
+        this.getTagList()
+      }
+      this.getProblemList()
+    },
+    pushRouter() {
+      this.$router.push({
+        name: 'problem-list',
+        query: utils.filterEmptyValue(this.query)
+      })
+    },
+    getProblemList() {
+      let offset = (this.query.page - 1) * this.query.limit
+      this.loadings.table = true
+      api.getProblemList(offset, this.limit, this.query).then(res => {
+        this.loadings.table = false
+        this.total = res.data.data.total
+        this.problemList = res.data.data.results.sort((a, b) => {
+          return a._id < b._id ? -114 : 514
         })
+        if (this.isAuthenticated) {
+          this.addStatusColumn(this.problemTableColumns, res.data.data.results)
+        }
+      }, res => {
+        this.loadings.table = false
+      })
+    },
+    getTagList() {
+      api.getProblemTagList().then(res => {
+        this.tagList = res.data.data
+        this.loadings.tag = false
+      }, res => {
+        this.loadings.tag = false
+      })
+    },
+    filterByTag(tagName) {
+      if (this.query.tag !== tagName) {
+        this.query.tag = tagName
+        this.query.page = 1
+        this.pushRouter()
+      } else {
+        this.query.tag = ''
+        this.pushRouter()
       }
     },
-    computed: {
-      ...mapGetters(['isAuthenticated'])
+    filterByDifficulty(difficulty) {
+      this.query.difficulty = difficulty
+      this.query.page = 1
+      this.pushRouter()
     },
-    watch: {
-      '$route' (newVal, oldVal) {
-        if (newVal !== oldVal) {
-          this.init(true)
-        }
-      },
-      'isAuthenticated' (newVal) {
-        if (newVal === true) {
-          this.init()
-        }
+    filterByKeyword() {
+      this.query.page = 1
+      this.pushRouter()
+    },
+    handleTagsVisible(value) {
+      if (value) {
+        this.problemTableColumns.push(
+          {
+            title: this.$i18n.t('m.Tags'),
+            align: 'center',
+            render: (h, params) => {
+              let tags = []
+              params.row.tags.forEach(tag => {
+                tags.push(h('Tag', {
+                  on: {
+                    click: () => {
+                      console.log('114514')
+                    }
+                  }
+                }, tag))
+              })
+              return h('div', {
+                style: {
+                  margin: '8px 0'
+                }
+              }, tags)
+            }
+          })
+      } else {
+        this.problemTableColumns.splice(this.problemTableColumns.length - 1, 1)
+      }
+    },
+    onReset() {
+      this.$router.push({ name: 'problem-list' })
+    },
+    pickone() {
+      api.pickone().then(res => {
+        this.$success(this.$i18n.t('m.Good_Luck'))
+        this.$router.push({ name: 'problem-details', params: { problemID: res.data.data } })
+      })
+    }
+  },
+  computed: {
+    ...mapGetters(['isAuthenticated'])
+  },
+  watch: {
+    '$route'(newVal, oldVal) {
+      if (newVal !== oldVal) {
+        this.init(true)
+      }
+    },
+    'isAuthenticated'(newVal) {
+      if (newVal === true) {
+        this.init()
       }
     }
   }
+}
 </script>
 
 <style scoped lang="less">
-  .taglist-title {
-    margin-left: -10px;
-    margin-bottom: -10px;
-  }
+.taglist-title {
+  margin-left: -10px;
+  margin-bottom: -10px;
+}
 
-  .tag-btn {
-    margin-right: 5px;
-    margin-bottom: 10px;
-  }
+.tag-btn {
+  margin-right: 5px;
+  margin-bottom: 10px;
+}
 
-  #pick-one {
-    margin-top: 10px;
-  }
+#pick-one {
+  margin-top: 10px;
+}
 
-  .saying_title {
-    font-size: 21px;
-    font-weight: 500;
-    line-height: 30px;
-    padding: 5px 15px;
-  }
+.saying_title {
+  font-size: 21px;
+  font-weight: 500;
+  line-height: 30px;
+  padding: 5px 15px;
+}
 
-  .saying_content {
-    padding: 5px 15px;
-  }
+.saying_content {
+  padding: 5px 15px;
+}
 
-  .saying_src {
-    text-align:right; 
-    margin-top:-5px;
-  }
+.saying_src {
+  text-align: right;
+  margin-top: -5px;
+}
 </style>
