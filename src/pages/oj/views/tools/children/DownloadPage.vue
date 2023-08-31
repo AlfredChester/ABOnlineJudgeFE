4451b6000b5136e00478bc7ffc10c3e3ae098eca
FredB-mine <fredbao1126@gmail.com>
2023-08-31 22:48:43 +0800

    DownloadPage

--- src/pages/oj/views/tools/children/DownloadPage.vue
+++ src/pages/oj/views/tools/children/DownloadPage.vue
@@ -108,303 +108,320 @@
 </template>
 
 <script>
-  import pdf from 'vue-pdf'
-  import 'viewerjs/dist/viewer.css'
-  import { mapGetters } from 'vuex'
-  import { api as viewerApi } from 'v-viewer'
-  import utils from '@/utils/utils'
-  
-  function requireListening(grade) {
-    return require(`../../../../../assets/Listenings/${grade}/${grade}.zip`)
-  }
+import pdf from 'vue-pdf'
+import 'viewerjs/dist/viewer.css'
+import { mapGetters } from 'vuex'
+import { api as viewerApi } from 'v-viewer'
+import utils from '@/utils/utils'
 
-  function requireGeography(grade) {
-    return require(`../../../../../assets/Geography/${grade}/index.zip`)
-  }
+function requireListening(grade) {
+  return require(`../../../../../assets/Listenings/${grade}/${grade}.zip`)
+}
 
-  function requireChinese(name) {
-    return require(`../../../../../assets/ChinesePack/${name}`)
-  }
+function requireGeography(grade) {
+  return require(`../../../../../assets/Geography/${grade}/index.zip`)
+}
 
-  function requirePdf(_title, sum) {
+function requireChinese(name) {
+  return require(`../../../../../assets/ChinesePack/${name}`)
+}
+
+function requirePdf(_title, sum) {
+  return {
+    title: _title,
+    src: '/static/Docs/' + _title + '.pdf',
+    pageSum: sum,
+    show: false,
+    currentPage: 1,
+    loadingRatio: 0,
+    downloadName: _title + '.pdf'
+  }
+}
+
+export default {
+  name: 'Downloads',
+  components: {
+    pdf
+  },
+  data() {
     return {
-      title: _title,
-      src: '/static/Docs/' + _title + '.pdf',
-      pageSum: sum,
-      show: false,
-      currentPage: 1,
-      loadingRatio: 0,
-      downloadName: _title + '.pdf'
-    }
-  }
-
-  export default {
-    name: 'Downloads',
-    components: {
-      pdf
-    },
-    data() {
-      return {
-        rootDomain: window.location.origin,
-        buttonSize: 'large',
-        Downloads: {
-          Exercises: {
-            title: '模拟卷/练习卷',
-            data: [],
-            pdfData: [
-              requirePdf('Unit 8   Reading ABCD', 7),
-              requirePdf('英语初一上学期期中考试-市北', 8),
-              requirePdf('7A 市北期中 听力文字及参考答案', 4)
-            ]
-          },
-          Listening: {
-            title: '听力部分',
-            data: [
-              {
-                title: '7A初中英语听力训练 zip压缩包',
-                src: requireListening('7A')
-              },
-              {
-                title: '7B初中英语听力训练 zip压缩包',
-                src: requireListening('7B')
-              }
-            ],
-            pdfData: []
-          },
-          Geography: {
-            title: '地理配套光盘',
-            data: [
-              {
-                title: '上海版地理（六年级第一、二学期）配套光盘-2021',
-                src: requireGeography(6)
-              },
-              {
-                title: '上海版地理（七年级第一、二学期）配套光盘-2022',
-                src: requireGeography(7)
-              }
-            ],
-            pdfData: []
-          },
-          Chinese: {
-            title: '语文学习资料包',
-            data: [
-              {
-                title: '现代文阅读笔记',
-                src: requireChinese('现代文阅读.jpg')
-              },
-              {
-                title: '期中考试复习范围',
-                src: requireChinese('mid-term.jpg')
-              },
-              {
-                title: '单元练习2家默范围及要求',
-                src: requireChinese('单元练习2家默范围及要求.jpg')
-              },
-              {
-                title: '期末考试复习范围',
-                src: requireChinese('7Afinal.jpg')
-              }
-            ],
-            pdfData: [
-              requirePdf('《两小儿辩日》（翻译＋内容理解）', 15),
-              requirePdf('《书戴嵩画牛》', 22),
-              requirePdf('《学弈》文学常识及翻译', 8),
-              requirePdf('《学弈》内容理解', 5),
-              requirePdf('《伯牙鼓琴》', 18),
-              requirePdf('《朝花夕拾》阅读任务单1', 2),
-              requirePdf('《陈太丘与友期行》（全）', 16),
-              requirePdf('《观沧海》课件', 12),
-              requirePdf('《天净沙•秋思》课件', 12),
-              requirePdf('《咏雪》（全）', 16),
-              requirePdf('《论语》十二章（全）', 21),
-              requirePdf('《诫子书》（全）', 17),
-              requirePdf('《诫子书》家默卷及参考答案', 2),
-              requirePdf('《狼》课件（全）', 32),
-              requirePdf('《狼》家默卷及答案', 3),
-              requirePdf('《穿井得一人》课件', 16),
-              requirePdf('《穿井得一人》家默卷', 1),
-              requirePdf('《孙权劝学》课件（全）', 29),
-              requirePdf('《孙权劝学》家默卷', 2),
-              requirePdf('《卖油翁》文常及翻译', 13),
-              requirePdf('《爱莲说》翻译', 10),
-              requirePdf('《陋室铭》翻译', 9),
-              requirePdf('《陋室铭》家默卷', 1),
-              requirePdf('《活板》 翻译部分', 25),
-              requirePdf('杞人忧天 课件', 17),
-              requirePdf('期中综合练习卷二', 5),
-              requirePdf('期中综合练习卷三', 5),
-              requirePdf('期中综合二及综合三现代文阅读参考答案', 1),
-              requirePdf('风华初级中学2021学年第一学期七年级语文阶段单元练习(3)', 4),
-              requirePdf('单元练习二文言文讲评', 14),
-              requirePdf('作文《我懂得了爱》写作指导', 3),
-              requirePdf('7A 语文复习本--by 吴宇桐', 8),
-              requirePdf('周末练习七', 2),
-              requirePdf('期中古诗家默卷A', 2),
-              requirePdf('期中古诗家默卷B', 2)
-            ]
-          },
-          truth: {
-            title: '真 理 诞 生 于 一 百 块 钱 之 后',
-            data: [],
-            pdfData: [
-              requirePdf('七上地理练习册答案', 7),
-              requirePdf('七下地理练习册答案', 14),
-              requirePdf('七年级上科学练习册答案', 68),
-              requirePdf('七年级下科学练习册答案', 27),
-              requirePdf('上海练习册道德与法治七年级（全一册）(7)', 16),
-              requirePdf('中国历史第一册练习部分参考答案', 16),
-              requirePdf('中国历史第二册练习部分参考答案', 12),
-              requirePdf('中国历史第三册练习部分参考答案', 20)
-            ]
-          }
+      rootDomain: window.location.origin,
+      buttonSize: 'large',
+      Downloads: {
+        Exercises: {
+          title: '模拟卷/练习卷',
+          data: [],
+          pdfData: [
+            requirePdf('Unit 8   Reading ABCD', 7),
+            requirePdf('英语初一上学期期中考试-市北', 8),
+            requirePdf('7A 市北期中 听力文字及参考答案', 4)
+          ]
         },
-        AllImage: [],
-        IgnoreTitle: '真 理 诞 生 于 一 百 块 钱 之 后'
-      }
-    },
-    mounted() {
-      console.log('[oj/DownloadPage.vue] Loaded DownloadPage.vue')
-      for (var _name in this.Downloads) {
-        for (var _file of this.Downloads[_name].data) {
-          if (_file.src.endsWith('.jpg')) {
-            this.AllImage.push(_file.src)
-            console.log(`[oj/DownloadPage.vue] Added ${_file.src} to AllImage`)
-          }
-        }
-      }
-    },
-    methods: {
-      previewImg(url) {
-        let ImageTemp = this.AllImage
-        let urlIdx = ImageTemp.findIndex(x => {
-          return x === url
-        })
-        if (process.env.NODE_ENV === 'development') {
-          console.log('[oj/DownloadPage.vue] Preview url:', url)
-          console.log('[oj/DownloadPage.vue] All images:', ImageTemp)
-          console.log('[oj/DownloadPage.vue] urlIdx:', urlIdx)
-        }
-        ImageTemp[urlIdx] = ImageTemp[0]
-        ImageTemp[0] = url
-        console.log('[oj/DownloadPage.vue] All images (sorted):', ImageTemp)
-        const $viewer = viewerApi({
-          images: ImageTemp
-        })
-        console.log('[oj/DownloadPage.vue] Viewer object:', $viewer)
-      },
-      print(obj) {
-        console.log(obj)
-      },
-      downloadPdf(obj) {
-        utils.realAxiosGet(
-          obj.src, {
-            responseType: 'blob'
-          }
-        ).then((res) => {
-          const url = window.URL.createObjectURL(res.data)
-          const link = document.createElement('a')
-          let fileName = obj.downloadName
-          console.log('[oj/DownloadPage.vue] fileName is', fileName)
-          link.href = url
-          link.setAttribute('download', fileName)
-          document.body.appendChild(link)
-          link.click()
-        })
-      },
-      goRoute(url) {
-        window.location.href = url
-      },
-      handleSwitching(title) {
-        for (var name in this.Downloads) {
-          this.Downloads[name].pdfData.forEach(element => {
-            if (element.title !== title) {
-              element.show = false
-            } else {
-              element.show = !element.show
-              console.log('[oj/DownloadPage.vue] loading ratio: ', element.loadingRatio)
+        Listening: {
+          title: '听力部分',
+          data: [
+            {
+              title: '7A初中英语听力训练 zip压缩包',
+              src: requireListening('7A')
+            },
+            {
+              title: '7B初中英语听力训练 zip压缩包',
+              src: requireListening('7B')
+            },
+            {
+              title: '8A初中英语听力训练 zip压缩包',
+              src: requireListening('8A')
             }
-          })
+          ],
+          pdfData: []
+        },
+        Geography: {
+          title: '地理配套光盘',
+          data: [
+            {
+              title: '上海版地理（六年级第一、二学期）配套光盘-2021',
+              src: requireGeography(6)
+            },
+            {
+              title: '上海版地理（七年级第一、二学期）配套光盘-2022',
+              src: requireGeography(7)
+            }
+          ],
+          pdfData: []
+        },
+        Chinese: {
+          title: '语文学习资料包',
+          data: [
+            {
+              title: '现代文阅读笔记',
+              src: requireChinese('现代文阅读.jpg')
+            },
+            {
+              title: '期中考试复习范围',
+              src: requireChinese('mid-term.jpg')
+            },
+            {
+              title: '单元练习2家默范围及要求',
+              src: requireChinese('单元练习2家默范围及要求.jpg')
+            },
+            {
+              title: '期末考试复习范围',
+              src: requireChinese('7Afinal.jpg')
+            }
+          ],
+          pdfData: [
+            requirePdf('《两小儿辩日》（翻译＋内容理解）', 15),
+            requirePdf('《书戴嵩画牛》', 22),
+            requirePdf('《学弈》文学常识及翻译', 8),
+            requirePdf('《学弈》内容理解', 5),
+            requirePdf('《伯牙鼓琴》', 18),
+            requirePdf('《朝花夕拾》阅读任务单1', 2),
+            requirePdf('《陈太丘与友期行》（全）', 16),
+            requirePdf('《观沧海》课件', 12),
+            requirePdf('《天净沙•秋思》课件', 12),
+            requirePdf('《咏雪》（全）', 16),
+            requirePdf('《论语》十二章（全）', 21),
+            requirePdf('《诫子书》（全）', 17),
+            requirePdf('《诫子书》家默卷及参考答案', 2),
+            requirePdf('《狼》课件（全）', 32),
+            requirePdf('《狼》家默卷及答案', 3),
+            requirePdf('《穿井得一人》课件', 16),
+            requirePdf('《穿井得一人》家默卷', 1),
+            requirePdf('《孙权劝学》课件（全）', 29),
+            requirePdf('《孙权劝学》家默卷', 2),
+            requirePdf('《卖油翁》文常及翻译', 13),
+            requirePdf('《爱莲说》翻译', 10),
+            requirePdf('《陋室铭》翻译', 9),
+            requirePdf('《陋室铭》家默卷', 1),
+            requirePdf('《活板》 翻译部分', 25),
+            requirePdf('杞人忧天 课件', 17),
+            requirePdf('期中综合练习卷二', 5),
+            requirePdf('期中综合练习卷三', 5),
+            requirePdf('期中综合二及综合三现代文阅读参考答案', 1),
+            requirePdf('风华初级中学2021学年第一学期七年级语文阶段单元练习(3)', 4),
+            requirePdf('单元练习二文言文讲评', 14),
+            requirePdf('作文《我懂得了爱》写作指导', 3),
+            requirePdf('7A 语文复习本--by 吴宇桐', 8),
+            requirePdf('周末练习七', 2),
+            requirePdf('期中古诗家默卷A', 2),
+            requirePdf('期中古诗家默卷B', 2)
+          ]
+        },
+        truth: {
+          title: '真 理 诞 生 于 一 百 块 钱 之 后',
+          data: [],
+          pdfData: [
+            requirePdf('七上地理练习册答案', 7),
+            requirePdf('七下地理练习册答案', 14),
+            requirePdf('七年级上科学练习册答案', 68),
+            requirePdf('七年级下科学练习册答案', 27),
+            requirePdf('上海练习册道德与法治七年级（全一册）(7)', 16),
+            requirePdf('中国历史第一册练习部分参考答案', 16),
+            requirePdf('中国历史第二册练习部分参考答案', 12),
+            requirePdf('中国历史第三册练习部分参考答案', 20)
+          ]
         }
       },
-      handlePageSwitching(element, mode) {
-        if (mode === 'up' && element.currentPage > 1) {
-          element.currentPage -= 1
-        } else if (mode === 'down' && element.currentPage < element.pageSum) {
-          element.currentPage += 1
-        } else {
-          this.$info('没有这一页哦 ^_^')
+      AllImage: [],
+      IgnoreTitle: '真 理 诞 生 于 一 百 块 钱 之 后'
+    }
+  },
+  mounted() {
+    console.log('[oj/DownloadPage.vue] Loaded DownloadPage.vue')
+    for (var _name in this.Downloads) {
+      for (var _file of this.Downloads[_name].data) {
+        if (_file.src.endsWith('.jpg')) {
+          this.AllImage.push(_file.src)
+          console.log(`[oj/DownloadPage.vue] Added ${_file.src} to AllImage`)
         }
       }
-    },
-    computed: {
-      ...mapGetters(['isAuthenticated', 'isAdminRole'])
     }
+  },
+  methods: {
+    previewImg(url) {
+      let ImageTemp = this.AllImage
+      let urlIdx = ImageTemp.findIndex(x => {
+        return x === url
+      })
+      if (process.env.NODE_ENV === 'development') {
+        console.log('[oj/DownloadPage.vue] Preview url:', url)
+        console.log('[oj/DownloadPage.vue] All images:', ImageTemp)
+        console.log('[oj/DownloadPage.vue] urlIdx:', urlIdx)
+      }
+      ImageTemp[urlIdx] = ImageTemp[0]
+      ImageTemp[0] = url
+      console.log('[oj/DownloadPage.vue] All images (sorted):', ImageTemp)
+      const $viewer = viewerApi({
+        images: ImageTemp
+      })
+      console.log('[oj/DownloadPage.vue] Viewer object:', $viewer)
+    },
+    print(obj) {
+      console.log(obj)
+    },
+    downloadPdf(obj) {
+      utils.realAxiosGet(
+        obj.src, { responseType: 'blob' }
+      ).then((res) => {
+        const url = window.URL.createObjectURL(res.data)
+        const link = document.createElement('a')
+        let fileName = obj.downloadName
+        console.log('[oj/DownloadPage.vue] fileName is', fileName)
+        link.href = url
+        link.setAttribute('download', fileName)
+        document.body.appendChild(link)
+        link.click()
+      })
+    },
+    goRoute(url) {
+      window.location.href = url
+    },
+    handleSwitching(title) {
+      for (var name in this.Downloads) {
+        this.Downloads[name].pdfData.forEach(element => {
+          if (element.title !== title) {
+            element.show = false
+          } else {
+            element.show = !element.show
+            console.log('[oj/DownloadPage.vue] loading ratio: ', element.loadingRatio)
+          }
+        })
+      }
+    },
+    handlePageSwitching(element, mode) {
+      if (mode === 'up' && element.currentPage > 1) {
+        element.currentPage -= 1
+      } else if (mode === 'down' && element.currentPage < element.pageSum) {
+        element.currentPage += 1
+      } else {
+        this.$info('没有这一页哦 ^_^')
+      }
+    }
+  },
+  computed: {
+    ...mapGetters(['isAuthenticated', 'isAdminRole'])
   }
+}
 </script>
 
 <style scoped lang="less">
-  .container {
-    margin-bottom: 20px;
-    .bigger_title {
-      font-size: xx-large; 
-      margin-top: 3px;
-    }
-    .content {
-      font-size: 16px;
-      margin: 0 50px 20px 50px;
-      >ul {
-        list-style: disc;
-        li {
-          line-height: 2;
-          .title {
-            font-weight: 500;
-          }
-        }
-      }
-      .MaterialType {
-        margin-bottom: 10px;
-        .Mat_title {
-          margin-top:15px; 
-          margin-bottom: 15px;
-        }
-        .type_row {
-          margin-bottom: 5px;
-          .ele_title {
-            margin-top:6px; 
-            margin-bottom: 6px;
-          }
-          .loading_div {
-            margin-top: -175px; 
-            text-align: center;
-            .img {
-              display: block;
-              width: 200px;
-              height: 150px;
-              top: 0;
-              left: 0;
-              right: 0;
-              bottom: 0;
-              margin: auto;
-              font-size: 18px;
-              color: rgb(1, 1, 1);
-            }
-          }
+.container {
+  margin-bottom: 20px;
+
+  .bigger_title {
+    font-size: xx-large;
+    margin-top: 3px;
+  }
+
+  .content {
+    font-size: 16px;
+    margin: 0 50px 20px 50px;
+
+    >ul {
+      list-style: disc;
+
+      li {
+        line-height: 2;
+
+        .title {
+          font-weight: 500;
         }
       }
     }
-    .demo-spin-icon-load {
-      animation: ani-demo-spin 1s linear infinite;
-    }
-    @keyframes ani-demo-spin {
-      from {
-        transform: rotate(0deg);
+
+    .MaterialType {
+      margin-bottom: 10px;
+
+      .Mat_title {
+        margin-top: 15px;
+        margin-bottom: 15px;
       }
-      50% {
-        transform: rotate(180deg);
-      }
-      to {
-        transform: rotate(360deg);
+
+      .type_row {
+        margin-bottom: 5px;
+
+        .ele_title {
+          margin-top: 6px;
+          margin-bottom: 6px;
+        }
+
+        .loading_div {
+          margin-top: -175px;
+          text-align: center;
+
+          .img {
+            display: block;
+            width: 200px;
+            height: 150px;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            margin: auto;
+            font-size: 18px;
+            color: rgb(1, 1, 1);
+          }
+        }
       }
     }
   }
+
+  .demo-spin-icon-load {
+    animation: ani-demo-spin 1s linear infinite;
+  }
+
+  @keyframes ani-demo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+
+    50% {
+      transform: rotate(180deg);
+    }
+
+    to {
+      transform: rotate(360deg);
+    }
+  }
+}
 </style>
