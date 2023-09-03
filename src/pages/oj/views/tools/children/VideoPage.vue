1e873299b2e91d6175d7bad90408a5a7ff8e9721
FredB-mine <fredbao1126@gmail.com>
2023-09-03 19:44:43 +0800

    VideoPage

---
+++ src/pages/oj/views/tools/children/VideoPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <panel class="container">
+    <div slot="title" style="font-size: xx-large; margin-top: 3px;">
+      {{ $t('m.VideoPage') }}
+    </div>
+    <div class="content">
+    </div>
+  </panel>
+</template>
+
+<script>
+export default {
+  name: 'VideoPage',
+  data() {
+    return {
+      shows: []
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.container {
+  margin-bottom: 20px;
+
+  .content {
+    font-size: 16px;
+    margin: 0 50px 20px 50px;
+  }
+}
+</style>
