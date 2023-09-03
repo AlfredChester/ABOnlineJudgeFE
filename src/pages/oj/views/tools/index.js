bc376c505ae1098616871b82e79fef9fd0318326
FredB-mine <fredbao1126@gmail.com>
2023-09-03 19:44:45 +0800

    index

--- src/pages/oj/views/tools/index.js
+++ src/pages/oj/views/tools/index.js
@@ -4,8 +4,9 @@ const DownloadPage = () => import(/* webpackChunkName: "tools" */ './children/Do
 const DuckChess = () => import(/* webpackChunkName: "tools" */ './children/DuckChess.vue')
 const HomoNumber = () => import(/* webpackChunkName: "tools" */ './children/HomoNumber.vue')
 const Listening = () => import(/* webpackChunkName: "tools" */ './children/Listening.vue')
+const VideoPage = () => import(/* webpackChunkName: "tools" */ './children/VideoPage.vue')
 
 export {
     Tools, CodeCompresser, DownloadPage,
-    DuckChess, HomoNumber, Listening
+    DuckChess, HomoNumber, Listening, VideoPage
 }
